8b3ef6f744d3d4516f044f6664958034dec89de4
areenatanim <areenatanim@gmail.com>
2025-03-11 08:10:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("arina is a good girl");
+console.log(document);
+console.log(document.body);
+const student = {
+                        name: 'arina',
+                        ID : 34,
+                        study : function (time) {
+                                console.log(time, 'doing study');
+                                
+                        }
+                }
+                console.log('coding from html page');
+
+
